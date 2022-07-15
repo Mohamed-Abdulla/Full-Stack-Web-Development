28f230be3915ed746af895c325f353b552d29cdc
mohamed-abdulla <abdabdulla78@gmail.com>
2022-07-15 11:39:07 +0530

    1st

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import { userInputs, productInputs } from "./formSource";
+import "./style/dark.scss";
+import { DarkModeContext } from "./context/darkModeContext";
+import { useContext } from "react";
+
+function App() {
+  const { darkMode } = useContext(DarkModeContext);
+  return (
+    //~REACT-ROUTER-DOM V6
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
